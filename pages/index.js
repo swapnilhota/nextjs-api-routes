b40f26f35ad0f9d3d71bee938b7c8d875d24af5f
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-26 10:56:29 +0530

    update-home-page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const HomePage = () => {
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows="5"></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} ></textarea>
         </div>
         <button>Send Feedback</button>
       </form>

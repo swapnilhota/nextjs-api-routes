dfc279e77070ce691f5ca09de1d0fcbf0e80d255
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-26 11:12:04 +0530

    sneding-request-feedback-api

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,19 @@ const HomePage = () => {
 
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback
+    };
+
+    fetch('/api/feedback', {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json()).then((data) => console.log(data));
   }
 
   return (

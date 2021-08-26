273f02f7a46e16cf5e183ccd575376041a4c3185
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-26 10:49:47 +0530

    update-home-page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,17 @@ const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5"></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   )
 }

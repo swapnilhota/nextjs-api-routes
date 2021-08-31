9580e88e5f1399fdc364cf36d74c0aa1ff983db5
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-31 09:59:41 +0530

    handling-get-request

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -22,7 +22,11 @@ function handler(req, res) {
         res.status(201).json({ message: "Succes", feedback: newFeedback });
     }
     else {
-        res.status(200).json({ message: "This works!" })
+        //GET request
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        res.status(200).json({ feedback: data });
     }
 }
 

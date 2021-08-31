66158a20c1e1e94850757852c11bffc2ffb5ebd6
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-31 10:31:21 +0530

    fetch-data-from-dynamic-api

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import fs from 'fs';
 import path from 'path';
 
 const FeedbackPage = (props) => {
+
+    const [feedbackData, setFeedbackData] = useState();
+
+    const loadFeedbackHandler = (id) => {
+        fetch(`/api/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFeedbackData(data.feedback);
+            })
+    }
+
     return (
-        <ul>
-            {props.feedbackItems.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ul>
+        <>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+                {props.feedbackItems.map((item) => (
+                    <li key={item.id}>
+                        {item.text} <button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+
     )
 }
 
